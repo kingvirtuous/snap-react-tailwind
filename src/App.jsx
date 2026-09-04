f91a737b5bce43e029489150b788b465076f85ad
kingvirtuous <kingvirtuous08@gmail.com>
2026-09-04 07:01:21 +0000

    exploring props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const App = () => {
+function Cat(props) {
+  
+  const {text, grear} = props
+  
   return (
+    <h1 className = "text-blue-200">My cat is good {grear}</h1>
+    
+   );
+}
+
+
+const App = () => {
+  return (<>
     <div className = "text-3xl text-white">hello</div>
+    <Cat text = "guy" grear="man"/>
+  </>
   )
 }
 
