b258d4d51aba89d21cbd335095f0dea07bfaaea1
kingvirtuous <kingvirtuous08@gmail.com>
2026-09-05 09:58:00 +0000

    stylingurtger

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const Button = () => {
   return (<>
     <div>
-    <button clas>Btn 1</button>
-      <button>Btn 2</button>
-      <button>Btn 3</button>
-      <button>Btn 4</button>
+    <button className="text-white m-4 py-5
+      px-9 border-2 border-white rounded-md">first</button>
+      <button className="bg-sky-400 text-4xl text-white m-5 px-10 py-5 rounded-lg">Second</button>
+      <button>Third</button>
+      <button>Fourth</button>
       </div>
     </>
   )
