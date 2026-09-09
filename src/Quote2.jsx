c2f76584322f06ee962ca49b03c1ad2838229c76
kingvirtuous <kingvirtuous08@gmail.com>
2026-09-09 16:22:19 +0000

    adding 2nd component

--- src/Quote2.jsx
+++ src/Quote2.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Quote2 = (props) => {
   const {quote} = props;
   return (
-    <div> class
-      <div>
-        <div>{quote.author}</div>
-        <div>{quote.bio}</div>
+    <div className="flex mt-6 p-8"> 
+      <div className="bg-blue-700 p-3 rounded-tl-lg rounded-bl-lg">
+        <div className="text-white text-3xl">{quote.author}</div>
+        <div className="text-sky-400">{quote.bio}</div>
       </div>
-      <div>{quote.text}</div>
+      <div className="text-white px-4 py-10 border border-white rounded-r-full">{quote.text}</div>
     </div>
   )
 }
