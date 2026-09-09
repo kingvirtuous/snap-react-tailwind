cd83d3e930be387b254a42e1ba6d5803ad69111e
kingvirtuous <kingvirtuous08@gmail.com>
2026-09-09 15:44:06 +0000

    getting started

--- src/Quote1.jsx
+++ src/Quote1.jsx
@@ -4,7 +4,11 @@ const Quote1 = (props) => {
   const {meee} = props;
   return (
     <div>
-      <div className="text-white">{meee.text}</div>
+      <div className="text-sky-100 bg-sky-500 py-8 px-7 w-92">{meee.text}</div>
+        <div className="bg-stone-300 px-[79px] py-3 w-92">
+          <div className="text-2xl text-sky-500">{meee.author}</div>
+          <div>{meee.bio}</div>
+        </div>
     </div>
   )
 }
