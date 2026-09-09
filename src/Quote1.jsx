7db8383a6aeaa153e0bd5a4f87f35bbeb4d4633a
kingvirtuous <kingvirtuous08@gmail.com>
2026-09-09 15:56:21 +0000

    finish first component

--- src/Quote1.jsx
+++ src/Quote1.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Quote1 = (props) => {
   const {meee} = props;
   return (
-    <div>
-      <div className="text-sky-100 bg-sky-500 py-8 px-7 w-92">{meee.text}</div>
-        <div className="bg-stone-300 px-[79px] py-3 w-92">
+    <div className=" items-center flex flex-col">
+      <div className="text-sky-100 bg-sky-500 py-8 px-7 rounded-t-lg w-92">{meee.text}</div>
+        <div className="bg-stone-300 px-[79px] py-2 w-92 rounded-b-lg border-2 border-blue-300">
           <div className="text-2xl text-sky-500">{meee.author}</div>
-          <div>{meee.bio}</div>
+          <div className="text-gray-400">{meee.bio}</div>
         </div>
     </div>
   )
